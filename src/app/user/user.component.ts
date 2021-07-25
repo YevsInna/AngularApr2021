04f14ff0b6a00ee54a39d309fc17cdd85cd3c21c
YevsInna <evseinka@gmail.com>
2021-07-25 17:28:45 +0300

    angular part 1

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {IUserModel} from "../models/UserModel";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent {
+@Input()
+userObj: IUserModel
+
+  constructor() { }
+
+
+
+}

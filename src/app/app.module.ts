65a0ae5f10e7fbe2d4a614d2470d44bf0647163f
YevsInna <evseinka@gmail.com>
2021-07-29 20:12:36 +0300

    lesson3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import {RouterModule, Routes} from "@angular/router";
+// import * as path from "path";
+import { UsersComponent } from './components/users/users.component';
+import {HttpClientModule} from "@angular/common/http";
+import { UserComponent } from './components/user/user.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import {UserGuardService} from "./servises/user-guard.service";
+
+let routes: Routes=[
+  {path: '', component: UsersComponent, canDeactivate: [UserGuardService]},
+  {path:'users/:id', component:UserDetailsComponent, canActivate:[UserGuardService],canDeactivate:[UserGuardService]},
+  {path: 'users', redirectTo:'', pathMatch: 'full'}
+  ];
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    UsersComponent,
+    UserComponent,
+    UserDetailsComponent,
+
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

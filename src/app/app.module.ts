656a496175e7c3a7fd0edfb44d05e14a31c4bee2
YevsInna <evseinka@gmail.com>
2021-08-03 18:34:26 +0300

    lesson5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { TdFormComponent } from './components/td-form/td-form.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
+import { DrivenFormComponent } from './components/driven-form/driven-form/driven-form.component';
+import {Route, RouterModule, Routes} from "@angular/router";
+import { UserComponent } from './components/user/user/user.component';
 
+let routes: Routes=[
+  {
+    path: 'https://jsonplaceholder.typicode.com/users', component: UserComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
+    TdFormComponent,
+    ReactiveFormComponent,
+    DrivenFormComponent,
+    UserComponent,
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]

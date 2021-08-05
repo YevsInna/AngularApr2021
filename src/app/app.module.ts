573328fa2037596ca4394686235db61e29f9e788
YevsInna <evseinka@gmail.com>
2021-08-05 16:54:46 +0300

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,28 +6,22 @@ import { TdFormComponent } from './components/td-form/td-form.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
 import { DrivenFormComponent } from './components/driven-form/driven-form/driven-form.component';
-import {Route, RouterModule, Routes} from "@angular/router";
-import { UsersComponent } from './components/user/users.component';
+import {HttpClientModule} from "@angular/common/http";
 
-let routes: Routes=[
-  {
-    path: 'https://jsonplaceholder.typicode.com/users', component: UsersComponent
-  }
-]
+
+
 @NgModule({
   declarations: [
     AppComponent,
     TdFormComponent,
     ReactiveFormComponent,
     DrivenFormComponent,
-    UsersComponent,
-
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(routes)
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/driven-form/driven-form/driven-form.component.ts
+++ src/app/components/driven-form/driven-form/driven-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IUser} from "../../../models/IUser";
 import {UserService} from "../../../services/user.service";
+
 
 @Component({
   selector: 'app-driven-form',
@@ -8,18 +9,19 @@ import {UserService} from "../../../services/user.service";
   styleUrls: ['./driven-form.component.scss']
 })
 export class DrivenFormComponent implements OnInit {
-user={
-  username: ''
-}
+  userInfo: IUser;
+  userId: number;
   users: IUser[];
 
-  constructor(private userService:UserService) { }
+  constructor(private userService: UserService) {
+  }
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe(value => this.users=value)
+    this.userService.getUsers().subscribe(value => this.users = value)
   }
 
   save(tref: HTMLFormElement) {
-
+    this.userService.getUserById(this.userId).subscribe(value => this.userInfo = value);
   }
+
 }

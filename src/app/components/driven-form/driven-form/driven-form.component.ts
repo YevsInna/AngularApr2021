4c5c241874919dc3410066fe817da17f7e20d48b
YevsInna <evseinka@gmail.com>
2021-08-05 16:07:25 +0300

    lesson_5

--- src/app/components/driven-form/driven-form/driven-form.component.ts
+++ src/app/components/driven-form/driven-form/driven-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {IUser} from "../../../models/IUser";
+import {UserService} from "../../../services/user.service";
 
 @Component({
   selector: 'app-driven-form',
@@ -12,9 +13,10 @@ user={
 }
   users: IUser[];
 
-  constructor() { }
+  constructor(private userService:UserService) { }
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe(value => this.users=value)
   }
 
   save(tref: HTMLFormElement) {
